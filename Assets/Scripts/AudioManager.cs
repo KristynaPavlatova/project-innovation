243fe96f0eb9ce524e6decc12fb82152e62a23bd
Kristýna Pavlátová <60941311+KristynaPavlatova@users.noreply.github.com>
2020-12-01 14:24:53 +0100

    Audio UI buttons

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource continueSound;//variable for the audio source you put in inspector
+
+    void Start()
+    {
+        continueSound = GetComponent<AudioSource>();//defining the audio source
+        CharacterCreationMenu.CharacterSubmited += playContinueSound;//once character is created and submitted
+    }
+    private void OnDestroy()
+    {
+        CharacterCreationMenu.CharacterSubmited -= playContinueSound;//unsubscibing the event you are listening to
+    }
+
+    private void playContinueSound()
+    {
+        continueSound.Play();//play the audio source
+    }
+}
